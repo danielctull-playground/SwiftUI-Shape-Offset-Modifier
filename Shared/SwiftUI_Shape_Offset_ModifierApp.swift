cbadf03cbc5c91c35d7de8cd178a4c6eda25d85e
Daniel Tull <dt@danieltull.co.uk>
2020-06-28 21:29:08 +0100

    Initial Commit

---
+++ Shared/SwiftUI_Shape_Offset_ModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Shape_Offset_ModifierApp.swift
+//  Shared
+//
+//  Created by Daniel Tull on 28.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Shape_Offset_ModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
